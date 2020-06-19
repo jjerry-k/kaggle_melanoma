d647ee54b77cabf8fce81cf0c48062a0a0ae76c5
jjerry-k <jaeyeol2931@gmail.com>
2020-06-19 15:30:10 +0000

    Update

--- 04_train_argumentation.py
+++ 04_train_argumentation.py
@@ -1,17 +1,63 @@
 # %%
-import os, random, time
+import os, random, time, argparse
 import numpy as np
 import pandas as pd
 import tensorflow as tf
 from matplotlib import pyplot as plt
 from sklearn.model_selection import train_test_split
 from tensorflow.keras import layers, models, losses, optimizers, callbacks
-from tensorflow.keras.applications import Xception
+from tensorflow.keras.applications import VGG16, VGG19, ResNet50, ResNet101, ResNet152, InceptionV3
+from tensorflow.keras.applications import Xception, MobileNet, MobileNetV2, ResNet50V2, ResNet101V2, ResNet152V2
+from tensorflow.keras.applications import InceptionResNetV2, DenseNet121, DenseNet169, DenseNet201
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 import wandb
 from wandb.keras import WandbCallback
 print("Package Loaded!")
+
+def str2bool(v):
+    if isinstance(v, bool):
+        return v
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError(f"Boolean value expected. {['yes', 'true', 't', 'y', '1'], ['no', 'false', 'f', 'n', '0']}")
+
+MODEL_LIST = {
+    'vgg16': VGG16, 
+    'vgg19': VGG19, 
+    'resnet50': ResNet50, 
+    'resnet101': ResNet101, 
+    'resnet152': ResNet152, 
+    'inceptionv3': InceptionV3, 
+    'xception': Xception, 
+    'mobilenet': MobileNet, 
+    'mobilenetv2': MobileNetV2, 
+    'resnet50v2': ResNet50V2, 
+    'resnet101v2': ResNet101V2, 
+    'resnet152v2': ResNet152V2, 
+    "inceptionresnetv2": InceptionResNetV2, 
+    'densenet121': DenseNet121, 
+    'densenet169': DenseNet169, 
+    'densenet201': DenseNet201}
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--gpu_number", type=str, help="GPU number to use")
+parser.add_argument("--model", type=str, help="Pretrained model to use")
+parser.add_argument("--freeze", type=str2bool, nargs='?', const=True, default=False,help="Freezing pretrained model")
+parser.add_argument("--epochs", type=int, help="Number of epochs")
+
+args = parser.parse_args()
+GPU_NUM = args.gpu_number
+EPOCHS = args.epochs
+FREEZE = args.freeze
+MODEL = args.model.lower()
+
+os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"] = GPU_NUM
+
 # %%
 # For Efficiency
 gpus = tf.config.experimental.list_physical_devices('GPU')
@@ -35,14 +81,13 @@ TE_IMG_PATH = os.path.join(ROOT, 'jpeg', 'test')
 SIZE = 224
 BATCH_SIZE = 1024
 SEED = 777
-EPOCHS=10
 
 random.seed(SEED)
 np.random.seed(SEED)
 tf.random.set_seed(SEED)
 
 date = time.ctime()[:-14].replace(' ', '_')
-wandb.init(project="kaggle_melanoma", name=date)
+wandb.init(project="kaggle_melanoma", name=f"{date}_{MODEL}_{FREEZE}")
 # %%
 tr_csv = pd.read_csv(TR_CSV_PATH)
 tr_csv['target']= tr_csv['target'].astype("str")
@@ -89,12 +134,16 @@ wandb_callback = WandbCallback(input_type='image', labels=[0,1], save_weights_on
 # %%
 strategy = tf.distribute.MirroredStrategy()
 with strategy.scope():
-    base_model = Xception(weights="imagenet", include_top=False, pooling='avg')
-    base_model.summary()
+    base_model = MODEL_LIST[MODEL](weights="imagenet", include_top=False, pooling='avg')
+
+    if FREEZE:
+        base_model.trainable = False
+    else:
+        base_model.trainable = True
 
     out = layers.Dense(1, activation="sigmoid")(base_model.output)
     model = models.Model(base_model.input, out)
-    model.compile(loss = 'binary_crossentropy', optimizer='adam', metrics=['acc'])
+    model.compile(loss = 'binary_crossentropy', optimizer=optimizers.SGD(learning_rate=0.001, momentum=0.99), metrics=['acc'])
 # %%
 model.fit(train_generator, \
             epochs=EPOCHS, \
@@ -110,5 +159,5 @@ result = model.predict(test_generator, verbose=1)
 SP_CSV_PATH = os.path.join(ROOT, "sample_submission.csv")
 sample_csv = pd.read_csv(SP_CSV_PATH)
 sample_csv['target'] = result
-sample_csv.to_csv("./submission.csv", index=False)
+sample_csv.to_csv(f"./{date}_submission_{MODEL}_{FREEZE}.csv", index=False)
 
--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # kaggle_melanoma
 ---
 
-- 1st: 0.442
+- 1st: 0.442 (Xception)
+- 2nd: 0.450 (Xception + Augmentation)
+- 3rd: 0.494 (MobileNetV2 + Augmentation)
